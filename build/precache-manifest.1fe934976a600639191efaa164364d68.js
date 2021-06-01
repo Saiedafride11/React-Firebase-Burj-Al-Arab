63297ac9f5a0276dda5195f6c0e27c06166900e0
Your Name <you@example.com>
2021-05-31 20:47:14 -0800

    first commit

---
+++ build/precache-manifest.1fe934976a600639191efaa164364d68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8329f94ed3d9275424af8f630e8befe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d7b2f3baf4fb3d4c24b",
+    "url": "/static/css/main.917c8f7b.chunk.css"
+  },
+  {
+    "revision": "7e2d706e450d8370190b",
+    "url": "/static/js/2.473ff73c.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/static/js/2.473ff73c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7b2f3baf4fb3d4c24b",
+    "url": "/static/js/main.0d0c409f.chunk.js"
+  },
+  {
+    "revision": "1dc93ba8951f2257dbd1",
+    "url": "/static/js/runtime-main.5ebb425c.js"
+  },
+  {
+    "revision": "50ce26577ffce4dddbfd23de347b70c8",
+    "url": "/static/media/header.50ce2657.png"
+  },
+  {
+    "revision": "5c655479124b8d9fd9bd8edcb3c6ccc7",
+    "url": "/static/media/logo.5c655479.png"
+  }
+]);
